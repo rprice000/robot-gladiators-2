e1de94b5282e02caf23b610a0bf604689fe24078
rprice000 <rtprice21@gmail.com>
2022-02-09 13:12:28 -0600

    Added alert line of javascript

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,2 @@
-// This is the javascript file
-
-// made change
+// Line 2 is used to test to see if javascript file is connected to the HTML File
+window.alert("This is an alert! Javascript is running!")
