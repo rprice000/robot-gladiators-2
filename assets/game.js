c56b7c423841fd8000f345e9e62e7bd472d8871d
rprice000 <rtprice21@gmail.com>
2022-02-10 09:41:19 -0600

    added console.log capability

--- assets/game.js
+++ assets/game.js
@@ -7,10 +7,20 @@
 // Line below takes input form the prompt and stores in as an object called playerName
 var playerName = window.prompt("What is your robot's name?");
 
+// Line below will return variable playerName in an alert box after the user inputs a name from the window.prompt
+// window.alert(playerName);
+// Line below will return variable playerName in the console after the user inputs a name from the window.prompt 
+console.log(playerName);
+
+// 3 Lines below show how console.log() can be used in different settings
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
 // Line below creates a function named "fight"
 function fight() {
     window.alert("The fight has begun!");
 }
 
 // Line below calls the fight function.  It must go below the function definition it calls.
-fight();
+// fight();
