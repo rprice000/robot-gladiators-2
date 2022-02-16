6622aa9934cbf8db96c4c9bcb3a063fd1cf8016e
rprice000 <rtprice21@gmail.com>
2022-02-16 13:24:04 -0600

    added end game logic

--- assets/game.js
+++ assets/game.js
@@ -117,7 +117,7 @@ var fight = function(enemyName) {
   }
 };
 
-console.log("Hello");
+
 // Line below calls the fight function.  Only one round with one enemy will be played
 // fight();
 
@@ -125,7 +125,20 @@ console.log("Hello");
 // A for loop is used to loop throught the enemyNames array.  This argument will be passed to the 
 // parameter in the fight function where the fight function is defined above.
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+    // pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+  }
 }
